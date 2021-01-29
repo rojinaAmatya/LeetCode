92aea8b4df3ec98a94bc4a32dc87d1a4b468940a
Rojina Amatya <rojinaamatya1432@gmail.com>
2021-01-29 13:45:08 -0600

    Create reverseString.py
    solved reverse string problem of LeetCode

---
+++ reverseString.py
@@ -0,0 +1,27 @@
+'''
+Write a function that reverses a string. The input string
+is given as an array of characters char[].
+
+Do not allocate extra space for another array,
+you must do this by modifying the input array in-place with O(1) extra memory.
+
+You may assume all the characters consist of printable ascii characters.
+
+Input: ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+
+'''
+
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        left =0
+        right = len(s)-1
+        while left < right:
+            s[left],s[right]= s[right],s[left]
+            left= left +1
+            right = right -1
+            
