9c5d93ce44a03aef728c2c743675f8aa6863ef4d
Rojina Amatya <rojinaamatya1432@gmail.com>
2021-06-03 16:36:03 -0500

    Solved leetcode problem #1662

---
+++ twoWordEquivalient.py
@@ -0,0 +1,16 @@
+'''
+
+Given two string arrays word1 and word2, return true if the two arrays
+represent the same string, and false otherwise.
+
+A string is represented by an array if the array elements concatenated
+in order forms the string.
+
+'''
+
+def arrayStringsAreEqual(word1, word2):
+    return ''.join(word1) == ''.join(word2)
+
+
+print(arrayStringsAreEqual(["ab","c"],["a","bc"]))
+    
